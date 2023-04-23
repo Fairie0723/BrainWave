c3025b0986ca90c46a82729fc880fd7afd31c7e9
Fairie B <fairie0723@gmail.com>
2023-04-23 13:39:59 -0500

    update from Jonathan.

---
+++ script.js
@@ -0,0 +1,9 @@
+function showForm() {
+    document.getElementById('prompt').style.display = 'block';
+    document.getElementById('add').style.display = 'none';
+}
+
+function hideForm() {
+    document.getElementById('prompt').style.display = 'none';
+    document.getElementById('add').style.display = 'block';
+}
